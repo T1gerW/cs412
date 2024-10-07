9dd3080308c78bc75e972210469d922bd6d50f32
Xing Wang <tigerw@bu.edu>
2024-10-07 16:55:14 -0400

    mini_fb app

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import *
+
+
+# class-based view 
+class ShowAllView(ListView):
+    '''the view to show all Articles'''
+    model = Profile
+    template_name = 'mini_fb/show_all_profiles.html'
+    context_object_name = 'profiles' #context variable used in template
